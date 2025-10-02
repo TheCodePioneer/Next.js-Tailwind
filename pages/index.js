7297cd813e7d9cdf73baccef9463fcf425f041c7
TheCodePioneer <150111051+TheCodePioneer@users.noreply.github.com>
2025-10-02 14:41:31 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-purple-600">
+        Hello Next.js + Tailwind!
+      </h1>
+    </div>
+  )
+}
